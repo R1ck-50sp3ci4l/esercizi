73f049010d36393568ffa809180618db1e0f73a8
Rick-MAuro010 <riccardo.morbidelli@campusdavinci.edu.it>
2025-10-07 11:02:17 +0200

    first commit , exercise about cards

---
+++ per casa.py
@@ -0,0 +1,16 @@
+"""
+1)generare una lista di numeri compresi tra 1 e 5 interi e
+poi fare una seconda lista di colori con i colori assegnati ai numeri
+"""
+import random
+colori={}
+colori={"nero":1;"rosso":2;"blu":3;"verde":4;"giallo":5}
+ph=[]
+for i in range(0,10):
+    numero=rand.randint(0,5)
+    ph_numero.append(numero)
+
+for in range(0,10):
+    colore=colori[ph_numero[i]]
+    ph_colori.append(colori)
+
